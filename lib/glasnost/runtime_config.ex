6511cbcb4b23b17a24b0c2c654025bb44d3bd23b
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2017-03-13 20:42:47 +0200

    add runtime checks for configs

--- lib/glasnost/runtime_config.ex
+++ lib/glasnost/runtime_config.ex
@@ -1,11 +1,11 @@
 defmodule RuntimeConfig do
 
   def blog_author do
-    System.get_env("GLASNOST_BLOG_AUTHOR")
+    System.get_env("GLASNOST_BLOG_AUTHOR") || throw("GLASNOST_BLOG_AUTHOR is NOT configured")
   end
 
   def source_blockchain do
-    System.get_env("GLASNOST_SOURCE_BLOCKCHAIN")
+    System.get_env("GLASNOST_SOURCE_BLOCKCHAIN") || throw("GLASNOST_SOURCE_BLOCKCHAIN is NOT configured")
   end
 
   def blockchain_client_mod do
