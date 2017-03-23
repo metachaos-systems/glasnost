39056df0dca49904e0ab37615af74164a6e7f159
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2017-03-23 15:28:43 +0200

    page controller test passing

--- test/web/controllers/page_controller_test.exs
+++ test/web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule Glasnost.Web.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Glasnost"
   end
 end
