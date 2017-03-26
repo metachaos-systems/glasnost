a62dacc05f88bcfc0aad9bf0ca5d80ff756331b0
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2017-03-26 15:41:45 +0300

    refactor orchestrator prototype

--- lib/glasnost/blockchain/sync_orchestrator.ex
+++ lib/glasnost/blockchain/sync_orchestrator.ex
@@ -7,16 +7,20 @@ defmodule Glasnost.Orchestrator.AuthorSyncSup do
   end
 
   def init(arg) do
-    author_configs = RuntimeConfig.get(:authors) |> AtomicMap.convert(safe: false)
-    Enum.each(author_configs, &validate_author_config/1)
-    children = for config <- author_configs do
-        worker(Wrk.AuthorSync, [config])
-    end
+    children = :authors
+      |> RuntimeConfig.get()
+      |> AtomicMap.convert(safe: true)
+      |> Enum.map(&validate_author_config/1)
+      |> Enum.map(&put_in(&1,[:source_blockchain], RuntimeConfig.get(:source_blockchain)))
+      |> Enum.map(&worker(Wrk.AuthorSync, [&1]) )
 
     supervise(children, strategy: :simple_one_for_one)
   end
 
   def validate_author_config(config) do
-     %{tags: %{blacklist: _, whitelist: _}, account_name: _, author_alias:_ } = config
+     # forcing a runtime pattern match exception if keys don't match
+     # TODO: add value validation
+     %{tags: %{blacklist: _, whitelist: _}, account_name: _, author_alias: _ } = config
   end
+
 end
