152866717d320e0d6bed308e819df340caa7303f
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2017-03-27 14:15:24 +0300

    patch version bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Glasnost.Mixfile do
 
   def project do
     [app: :glasnost,
-     version: "0.6.0",
+     version: "0.6.1",
      elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
