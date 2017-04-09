9173f6a9167c680a650a76505fb07fe258ecc223
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2017-04-09 18:12:50 +0300

    update deps

--- mix.exs
+++ mix.exs
@@ -29,7 +29,7 @@ defmodule Glasnost.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "1.3.0-rc.1"},
+    [{:phoenix, "1.3.0-rc.1", override: true},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_ecto, "~> 3.2"},
      {:postgrex, ">= 0.0.0"},
