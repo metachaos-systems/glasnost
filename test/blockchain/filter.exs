51894a9c4e570731974f9fb098d665a26550c035
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2017-04-16 13:04:07 +0300

    update tests for title filters

--- test/blockchain/filter.exs
+++ test/blockchain/filter.exs
@@ -5,14 +5,16 @@ defmodule Glasnost.PostImportFilterTest do
   test "title regex whitelist works correctly" do
     titles = ["SomeTitle", "", "1337", "Some Title"]
      |> Enum.map(& %{"title" => &1})
-    config = %{whitelist: ["SomeTitle", "1337"], blacklist: []}
-    assert [%{"title" => "SomeTitle"}, %{"title" => "1337"}] == AuthorSync.filter_by_title(titles, config)
+    whitelist = ["SomeTitle", "1337"]
+    assert [%{"title" => "SomeTitle"}, %{"title" => "1337"}] == Enum.filter(titles, &AuthorSync.matches_title_rule?(&1, :whitelist, whitelist))
   end
 
   test "title regex blacklist works correctly" do
-    titles = ["SomeTitle", "", "1337", "Some Title"]
-     |> Enum.map(& %{"title" => &1})
-    config = %{whitelist: [], blacklist: ["SomeTitle", "1337"]}
-    assert [%{"title" => ""}, %{"title" => "Some Title"}] == AuthorSync.filter_by_title(titles, config)
+    titles = ["SomeTitle", "", "1337", "Some Title"] |> Enum.map(& %{"title" => &1})
+    blacklist = ["SomeTitle", "1337"]
+    filtered = titles
+      |> Enum.reject(&AuthorSync.matches_title_rule?(&1, :blacklist, blacklist))
+      |> Enum.map(& &1["title"])
+    assert ["",  "Some Title"] == filtered
   end
 end
