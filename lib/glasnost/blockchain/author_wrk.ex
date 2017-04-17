4769723e196a94609b9624a8dd106000a00be576
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2017-04-17 12:57:09 +0300

    update workers

--- lib/glasnost/blockchain/author_wrk.ex
+++ lib/glasnost/blockchain/author_wrk.ex
@@ -1,6 +1,6 @@
 defmodule Glasnost.Worker.AuthorSync do
   use GenServer
-  alias Glasnost.{Repo, RuntimeConfig}
+  alias Glasnost.{Repo, RuntimeConfig, PostFilters, Post}
   import Logger
 
   def start_link(args, opts \\ []) do
@@ -28,11 +28,9 @@ defmodule Glasnost.Worker.AuthorSync do
      posts = posts
       |> Enum.map(&parse_json_metadata/1)
       |> Enum.map(&extract_put_tags/1)
-      |> Enum.filter(&matches_tag_rule?(&1, :whitelist, filters[:tags][:whitelist]))
-      |> Enum.reject(&matches_tag_rule?(&1, :blacklist, filters[:tags][:blacklist]))
-      |> Enum.filter(&matches_title_rule?(&1, :whitelist, filters[:title][:whitelist]))
-      |> Enum.reject(&matches_title_rule?(&1, :blacklist, filters[:title][:blacklist]))
-      |> Enum.filter(&matches_created_rule?(&1, filters[:created]))
+      |> Post.filter_by(:tags, filters[:tags])
+      |> Post.filter_by(:title, filters[:title])
+      |> Post.filter_by(:created, filters[:created])
 
      for post <- posts do
        save_to_db(post)
@@ -41,46 +39,7 @@ defmodule Glasnost.Worker.AuthorSync do
      {:noreply, state}
   end
 
-  def matches_tag_rule?(_, :whitelist,  []), do: true
-  def matches_tag_rule?(_, :whitelist,  nil), do: true
-  def matches_tag_rule?(_, :blacklist,  []), do: false
-  def matches_tag_rule?(_, :blacklist,  nil), do: false
-  def matches_tag_rule?(post, _, rules) when is_list(rules)  do
-    tags_set = Enum.into(post["tags"], MapSet.new)
-    filter_set = Enum.into(rules, MapSet.new)
-    !MapSet.disjoint?(tags_set, filter_set)
-  end
 
-  def matches_created_rule?(_, nil), do: true
-  def matches_created_rule?(_, %{only_after: "", only_before: ""}), do: true
-  def matches_created_rule?(post, %{only_after: only_after, only_before: only_before}) do
-    # offset is always zero, but required by ISO standard
-    only_after = if only_after == "", do:  "1970-01-01", else: only_after
-    only_before = if only_before == "", do:  "2038-01-01", else: only_before
-    {:ok, only_after, 0} = DateTime.from_iso8601(only_after <> "T00:00:00Z")
-    {:ok, only_before, 0} = DateTime.from_iso8601(only_before <> "T00:00:00Z")
-    {:ok, created, 0} = DateTime.from_iso8601(post["created"] <> "Z")
-    DateTime.compare(created, only_after) === :gt and DateTime.compare(created, only_before) === :lt
-  end
-
-  def matches_title_rule?(_, :whitelist, nil), do: true
-  def matches_title_rule?(_, :whitelist, []), do: true
-  def matches_title_rule?(_, :blacklist, nil), do: false
-  def matches_title_rule?(_, :blacklist, []), do: false
-  def matches_title_rule?(post, _, regex_strings) when is_list(regex_strings) do
-    regexes = Enum.reduce(regex_strings, [], fn regex_str, acc ->
-      case Regex.compile(regex_str) do
-         {:ok, regex} -> acc ++ [regex]
-         {:error, _} -> acc
-      end
-    end)
-    title = post["title"]
-    Enum.reduce(regexes, false, fn regex, acc -> acc or String.match?(title, regex) end)
-  end
-
-  def trim_trailing_ms(date) when is_bitstring(date) do
-     String.replace(date, ~r/\..+/, "")
-  end
 
   def parse_json_metadata(post) do
      result = case Poison.Parser.parse(post["json_metadata"]) do
@@ -92,6 +51,10 @@ defmodule Glasnost.Worker.AuthorSync do
 
   def extract_put_tags(post) do
      put_in(post, ["tags"], post["json_metadata"]["tags"])
+  end
+
+  def trim_trailing_ms(date) when is_bitstring(date) do
+     String.replace(date, ~r/\..+/, "")
   end
 
   def iterate(posts, state) do
