711522aef0c1422ff8af1c71ea30176994d35ac7
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2017-06-05 17:54:53 +0300

    dry out current page assigns

--- lib/glasnost/web/controllers/page_controller.ex
+++ lib/glasnost/web/controllers/page_controller.ex
@@ -3,14 +3,15 @@ defmodule Glasnost.Web.PageController do
   import Ecto.Query
   @posts_per_page 24
 
+  plug :extract_and_put_page_num
+
   def index(conn, params) do
-    page_num = extract_page_num(params)
     q = from c in Glasnost.Post
     posts = q
       |> Glasnost.Repo.all()
       |> Enum.sort(&(&1.unix_epoch >= &2.unix_epoch))
-      |> paginate_naively(page_num)
-    render conn, "posts.html", posts: posts, current_page: page_num
+      |> paginate_naively(conn.assigns.page_num)
+    render conn, "posts.html", posts: posts
   end
 
   def show(conn, %{"permlink" => permlink, "author" => author }) do
@@ -21,36 +22,40 @@ defmodule Glasnost.Web.PageController do
   end
 
   def tags(conn, params = %{"tag" => tag}) do
-    page_num = extract_page_num(params)
     q = from c in Glasnost.Post,
       order_by: [desc: c.id]
     posts = q
       |> Glasnost.Repo.all()
       |> Enum.filter(& tag in &1.tags)
-      |> paginate_naively(page_num)
-    render conn, "posts.html", posts: posts, current_page: page_num
+      |> paginate_naively(conn.assigns.page_num)
+    render conn, "posts.html", posts: posts
   end
 
   def authors(conn, params = %{"author" => author_name}) do
-    page_num = extract_page_num(params)
     q = from c in Glasnost.Post,
       where: c.author == ^author_name,
       order_by: [desc: c.id]
 
     posts = q
       |> Glasnost.Repo.all()
-      |> paginate_naively(page_num)
-    render conn, "posts.html", posts: posts, current_page: page_num
+      |> paginate_naively(conn.assigns.page_num)
+    render conn, "posts.html", posts: posts
   end
 
-  def extract_page_num(params) do
-    res = params
-     |> Map.get("page", "")
-     |> Integer.parse()
+  def extract_and_put_page_num(conn, params) do
+    case params do
+      [] ->
+        assign(conn, :page_num, 1)
+      true ->
+        res = params
+         |> Map.get("page", "")
+         |> Integer.parse()
 
-    case res do
-      {int, _} -> int
-      :error -> 1
+        case res do
+          {int, _} -> int
+          :error -> 1
+        end
+        assign(conn, :page_num, res)
     end
   end
 
