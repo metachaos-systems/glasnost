86bdbee9f569520abd63896cd41414a92b49e09b
ontofractal <ontofractal@protonmail.com>
2017-06-10 16:16:27 +0100

    first broadcaster iteration

--- lib/glasnost/web/channels/broadcaster.ex
+++ lib/glasnost/web/channels/broadcaster.ex
@@ -1,6 +1,19 @@
 defmodule Glasnost.ChannelBroadcaster do
-
-  def send(event) do
-    IO.inspect(event)
+  alias Glasnost.Web.Endpoint
+  def send(event, blockchain: :steem) do
+    alias Steemex.StructuredOps.{Comment, Vote}
+    {ev_data, ev_metadata} = event
+    event_type = case ev_data do
+      %Comment{} -> "new_comment"
+      _ -> nil
+    end
+    if event_type do
+      Endpoint.broadcast! "channel:steem_events", event_type, ev_data
+    end
   end
+
+  def send(event, :golos) do
+    Glasnost.Endpoint.broadcast! "room:golos", "new_event", event
+  end
+
 end
