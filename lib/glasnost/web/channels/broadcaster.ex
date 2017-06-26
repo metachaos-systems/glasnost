f68584254ea934ffc9a6acfc049577bbee246cb1
ontofractal <ontofractal@protonmail.com>
2017-06-26 18:38:50 +0100

    update broadcaster to match new event shape

--- lib/glasnost/web/channels/broadcaster.ex
+++ lib/glasnost/web/channels/broadcaster.ex
@@ -2,33 +2,11 @@ defmodule Glasnost.ChannelBroadcaster do
   alias Glasnost.Web.Endpoint
   @golos_events "channel:golos_events"
   @steem_events "channel:steem_events"
+  @steem_meta %{blockchain: :steem, type: :comment}
 
-  def send(event, blockchain: :steem) do
-    alias Steemex.StructuredOps.{Comment}
-    {ev_data, ev_metadata} = event
-    {event_type, ev_data, ev_metadata} = case ev_data do
-      %Comment{author: author, permlink: permlink} ->
-        {:ok, comment} = Steemex.get_content(author, permlink)
-        {"new_comment", comment, ev_metadata}
-      _ -> {nil, nil, nil}
-    end
-    if event_type do
-      Endpoint.broadcast! @steem_events, event_type, ev_data
-    end
-  end
-
-  def send(event, blockchain: :golos) do
-    alias Golos.StructuredOps.{Comment}
-    {ev_data, ev_metadata} = event
-    {event_type, ev_data, ev_metadata} = case ev_data do
-      %Comment{author: author, permlink: permlink} ->
-        {:ok, comment} = Golos.get_content(author, permlink)
-        {"new_comment", comment, ev_metadata}
-      _ -> {nil, nil, nil}
-    end
-    if event_type do
-      Endpoint.broadcast! @golos_events, event_type, ev_data
-    end
+  def send(%{data: %{author: author, permlink: permlink}, metadata: @steem_meta}) do
+    {:ok, comment} = Steemex.get_content(author, permlink)
+    Endpoint.broadcast! @steem_events, {"new_comment", comment}
   end
 
 
