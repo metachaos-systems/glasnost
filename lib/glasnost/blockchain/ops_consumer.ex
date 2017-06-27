474aadc437fd7e6f19830ab1ab327214e8fa8b3f
ontofractal <ontofractal@protonmail.com>
2017-06-27 13:46:43 +0100

    realtime sync prototype

--- lib/glasnost/blockchain/ops_consumer.ex
+++ lib/glasnost/blockchain/ops_consumer.ex
@@ -8,36 +8,32 @@ defmodule Glasnost.Steemlike.OpsConsumer do
 
   def init(%{config: config} = args \\ []) do
     Logger.info("Ops consumer for #{config.token} is initializing...")
-    schema = state.config.schema
-    config = state.config
+    schema = config.schema
+    new_config = config
       |> Map.put(:vote_schema, Module.concat(schema, Vote))
       |> Map.put(:comment_schema, Module.concat(schema, Comment))
     :timer.send_interval(10_000, :process_queue)
-    {:consumer, %{config: state.config}, subscribe_to: args.subscribe_to}
+    {:consumer, %{config: new_config}, subscribe_to: args.subscribe_to}
   end
 
   def handle_events(events, _from, state) do
-    comments = for event <- event, &(event.metadata.type === :comment) do
-      event
+    client_mod = state.config.client_mod
+    comments_to_update_new = events
+      |> Enum.filter(& &1.metadata.type === :comment)
+      |> Enum.map(&Map.take([:author, :permlink]))
+
+    comments_to_update_votes = events
+      |> Enum.filter(& &1.metadata.type === :vote)
+      |> Enum.map(&Map.take([:author, :permlink]))
+
+    commments_to_update = comments_to_update_new ++ comments_to_update_votes
+      |> Enum.uniq
+
+    for %{author: a, permlink: p} <- comments_to_update_new do
+      client_mod.get_data_and_update(a, p)
     end
-    votes = for event <- event, &(event.metadata.type === :vote) do
-      event
-    end
+
     {:noreply, [], state}
   end
-
-  def handle_info(%{data: data, metadata: %{type: :comment}}, state) do
-    {:noreply, [], state}
-  end
-
-  def handle_info(%{data: %{author: a, permlink: p}, metadata: %{type: :vote}}, state) do
-    {:noreply, [], state}
-  end
-
-
-  def handle_info(msg, state) do
-    {:noreply, [], state}
-  end
-
 
 end
