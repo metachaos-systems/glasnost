fe30f1cc994c5fe3266cdb4adb2d39aa14f1dfc3
ontofractal <ontofractal@protonmail.com>
2017-06-28 11:13:38 +0100

    update controller to work for both steem and golos chains

--- lib/glasnost/web/controllers/comment_controller.ex
+++ lib/glasnost/web/controllers/comment_controller.ex
@@ -3,23 +3,32 @@ defmodule Glasnost.Web.CommentController do
   import Ecto.Query
   @posts_per_page 24
 
+  plug :choose_schema
+
   def index(conn, params) do
-    q = from c in Glasnost.Golos.Comment
+    q = from c in conn.assigns.comment_schema
     comments = Glasnost.Repo.all(q)
       |> Enum.map(&Map.from_struct/1)
       |> Enum.map(&Map.drop(&1, [:__meta__]))
-    IO.inspect comments
     json conn, comments
   end
 
   def show(conn, %{"id" => id}) do
     [author, permlink] = String.split(id, "/")
-    comment = Glasnost.Repo.get_by(Glasnost.Golos.Comment, author: author, permlink: permlink)
+    comment = Glasnost.Repo.get_by(conn.assigns.comment_schema, author: author, permlink: permlink)
     if comment do
       json conn, comment
     else
       send_resp(conn, 404, "")
     end
+  end
+
+  def choose_schema(conn, _params) do
+    schema = case String.slice(conn.request_path, 1, 5) do
+      "golos" -> Glasnost.Golos.Comment
+      "steem" -> Glasnost.Steem.Comment
+    end
+    assign conn, :comment_schema, schema
   end
 
 end
