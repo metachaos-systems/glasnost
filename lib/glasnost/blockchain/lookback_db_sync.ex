4b95ec2a2945e8df0377430134b7529cb056799d
ontofractal <ontofractal@protonmail.com>
2017-06-29 12:12:58 +0100

    getting closer

--- lib/glasnost/blockchain/lookback_db_sync.ex
+++ lib/glasnost/blockchain/lookback_db_sync.ex
@@ -1,6 +1,7 @@
 defmodule Glasnost.Stage.LookbackBlocks do
   use GenStage
   alias Glasnost.Repo
+  @blocks_per_tick 100
 
   def start_link(args, options) do
     GenStage.start_link(__MODULE__, args, options)
@@ -19,13 +20,18 @@ defmodule Glasnost.Stage.LookbackBlocks do
 
   def handle_info(:next_blocks, state) do
     import Ecto.Query
-    tasks = for height <- state.current_starting_block..state.current_starting_block - 100 do
+    tasks = for height <- state.current_block..state.current_block - @blocks_per_tick do
       Task.async(fn ->
         {:ok, block} = state.client_mod.get_block(height)
+        block
       end)
     end
     blocks = Task.yield_many(tasks, 30_000)
+    blocks = for {:ok, block}  <- blocks do
+      block
+    end
     Process.send_after(self(), :next_blocks, 3_000)
+    state = put_in(state.config.current_block, state.config.current_block - @blocks_per_tick)
     {:noreply, blocks, state}
   end
 
