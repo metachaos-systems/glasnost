ab92449308cfe1b229080ef0484eb80833b0b94f
ontofractal <ontofractal@protonmail.com>
2017-07-25 15:08:20 +0100

    update router

--- lib/glasnost/web/router.ex
+++ lib/glasnost/web/router.ex
@@ -7,7 +7,6 @@ defmodule Glasnost.Web.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
-    plug Glasnost.Plugs.PutConfig
   end
 
   pipeline :api do
