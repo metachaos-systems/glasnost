f1772c0008a36a056dafe170969eed2167899030
ontofractal <ontofractal@protonmail.com>
2017-07-26 17:32:04 +0100

    temporarily revert to elixir 1.4

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM elixir:1.5
+FROM elixir:latest
 
 ENV DEBIAN_FRONTEND=noninteractive
 
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Glasnost.Mixfile do
   def project do
     [app: :glasnost,
      version: "0.11.0",
-     elixir: "~> 1.5",
+     elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
