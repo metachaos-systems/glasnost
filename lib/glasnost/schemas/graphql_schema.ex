19718fdb6c7e3a0d8379ff67160fca65d3a5b49b
ontofractal <ontofractal@protonmail.com>
2017-07-28 16:55:29 +0100

    fix graphql schema

--- lib/glasnost/schemas/graphql_schema.ex
+++ lib/glasnost/schemas/graphql_schema.ex
@@ -1,14 +1,14 @@
 defmodule Glasnost.Schema do
   use Absinthe.Schema
-  import_types Blog.Schema.Types
+  import_types Glasnost.Schema.Types
 
   query do
     field :steem_comments, list_of(:comment) do
-      resolve &Glasnost.CommentResolver.all/2
+      resolve fn _args, info -> Glasnost.CommentResolver.all(%{blockchain: :steem}, info) end
     end
 
     field :golos_comments, list_of(:comment) do
-      resolve &Glasnost.CommentResolver.all/2
+      resolve fn _args, info -> Glasnost.CommentResolver.all(%{blockchain: :golos}, info) end
     end
   end
 
