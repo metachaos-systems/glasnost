570473cad2106339805af15d38527bc894002c9b
ontofractal <ontofractal@protonmail.com>
2017-07-28 17:23:41 +0100

    add comment object

--- lib/glasnost/schemas/graphql_schema.ex
+++ lib/glasnost/schemas/graphql_schema.ex
@@ -7,6 +7,10 @@ defmodule Glasnost.Schema do
       resolve &Glasnost.CommentResolver.all/2
     end
 
+    field :comment, type: :comment do
+      arg :id, non_null(:id)
+      resolve &Glasnost.CommentResolver.find/2
+    end
 
   end
 
