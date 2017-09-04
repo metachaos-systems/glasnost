3e02cca676531269762da84a20f70ded8efbad0e
ontofractal <ontofractal@protonmail.com>
2017-09-04 17:02:59 +0100

    fix paths

--- lib/glasnost_web/controllers/page_controller.ex
+++ lib/glasnost_web/controllers/page_controller.ex
@@ -1,12 +1,8 @@
 defmodule GlasnostWeb.PageController do
   use GlasnostWeb, :controller
   import Ecto.Query
-  @posts_per_page 24
 
-  plug :extract_and_put_page_num
-
-
-  def index(conn, params) do
+  def index(conn, _params) do
     render conn, "index.html"
   end
 
--- lib/glasnost_web/router.ex
+++ lib/glasnost_web/router.ex
@@ -22,23 +22,13 @@ defmodule GlasnostWeb.Router do
 
   scope "/api", GlasnostWeb do
     pipe_through :api
-
-    get "/golos/comments/search", CommentController, :search
-    get "/golos/comments/stats", CommentController, :stats
-    get "/golos/comments/:author/:permlink", CommentController, :show
-    # resources "/steem/comments", CommentController
-    # resources "/golos/comments", CommentController
   end
 
   scope "/", GlasnostWeb do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
-    get "/streaming", PageController, :streaming_demo
-    get "/admin", AdminController, :index
-    get "/admin/onboarding", AdminController, :onboarding
-    post "/admin/mark_password_as_saved", AdminController, :mark_password_as_saved
-    post "/admin/command_and_control", AdminController, :command_and_control
+    # get "/streaming", PageController, :streaming_demo
   end
 
   # Other scopes may use custom stacks.
