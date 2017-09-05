6eeed932c95c72b2c7e3cfae198a0d336dd37d69
ontofractal <ontofractal@protonmail.com>
2017-09-05 11:45:56 +0100

    improve resolvers

--- lib/glasnost/resolvers/comments_resolver.ex
+++ lib/glasnost/resolvers/comments_resolver.ex
@@ -1,12 +1,18 @@
 defmodule Glasnost.CommentResolver do
   alias Glasnost.Repo
+  import Ecto.Query
 
-  def all(%{blockchain: blockchain}, _info) do
+  def all(%{blockchain: blockchain, author: author}, _info) do
     schema = case blockchain do
       "steem" -> Glasnost.Steem.Comment
       "golos" -> Glasnost.Golos.Comment
     end
-    {:ok, Repo.all(schema)}
+    q = if author do
+      from c in schema, where: c.author === ^author, order_by: [desc: c.created]
+    else
+      from c in schema, order_by: [desc: c.created], limit: 1_000
+    end
+    {:ok, Repo.all(q)}
   end
 
   def find(%{"blockchain" => blockchain, author: a, permlink: p}, _info) do
