22a8cd6ab97f31bc1689721454debfb14d54a4a8
ontofractal <ontofractal@protonmail.com>
2017-09-10 17:43:04 +0100

    fix excessive requests bug

--- lib/glasnost/schemas/steemlike_comment.ex
+++ lib/glasnost/schemas/steemlike_comment.ex
@@ -8,26 +8,26 @@ defmodule Glasnost.Steemlike.Comment do
       :golos -> {Glasnost.Golos.Comment, Golos}
     end
     existing_comment = Repo.get_by(schema_mod, author: author, permlink: permlink)
-    should_be_updated = is_nil(existing_comment) or (existing_comment and source !== :resync)
-    # TODO: improve update fn to exclude objects already in DB and matching certain clauses
-    # unless existing_comment && existing_comment.updated_at > NaiveDateTime.utc_now
-    {:ok, new_comment_data} = client_mod.get_content(author, permlink)
-    if new_comment_data.id !== 0 and should_be_updated do
-      cleaned_comment_data = new_comment_data |> add_timestamps()
-      result =
-        case existing_comment do
-            nil  -> struct(schema_mod, %{id: cleaned_comment_data.id})
-            comment -> comment
-          end
-          |> schema_mod.changeset(cleaned_comment_data)
-          |> Glasnost.Repo.insert_or_update
+    should_be_updated = is_nil(existing_comment) or (source !== :resync)
+    if should_be_updated do
+      {:ok, new_comment_data} = client_mod.get_content(author, permlink)
+      if new_comment_data.id !== 0 do
+        cleaned_comment_data = new_comment_data |> add_timestamps()
+        result =
+          case existing_comment do
+              nil  -> struct(schema_mod, %{id: cleaned_comment_data.id})
+              comment -> comment
+            end
+            |> schema_mod.changeset(cleaned_comment_data)
+            |> Glasnost.Repo.insert_or_update
 
-      case result do
-        {:ok, struct}       ->
-          :noop
-        {:error, changeset} ->
-          Logger.error("Persistence failed:")
-          Logger.error(inspect changeset)
+        case result do
+          {:ok, struct}       ->
+            :noop
+          {:error, changeset} ->
+            Logger.error("Persistence failed:")
+            Logger.error(inspect changeset)
+        end
       end
     end
   end
