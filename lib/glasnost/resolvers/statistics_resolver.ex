03c504c0d2f8c920138f4e7156d6301b02746d50
ontofractal <ontofractal@protonmail.com>
2017-09-26 19:14:40 +0100

    fix statistics resolver

--- lib/glasnost/resolvers/statistics_resolver.ex
+++ lib/glasnost/resolvers/statistics_resolver.ex
@@ -1,9 +1,10 @@
 defmodule Glasnost.StatisticsResolver do
   alias Glasnost.Repo
   import Ecto.Query
-  import Glasnost.ResolverUtils, only: [select_schema: 2]
+  import Glasnost.ResolverUtils, only: [select_schema: 1]
 
   def all(%{blockchain: blockchain}, _info) do
+    schema = select_schema(blockchain)
     {:ok, %{
       post_count: Repo.count(from c in schema, where: is_nil(c.parent_author)),
       comment_count: Repo.count(from c in schema, where: not is_nil(c.parent_author)),
