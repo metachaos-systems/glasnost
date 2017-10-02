687f68d0b107ff3828c1aa1b1e0b1eceb4bf7d37
ontofractal <ontofractal@protonmail.com>
2017-10-02 17:12:19 +0100

    bugfix

--- lib/glasnost/resolvers/utils.ex
+++ lib/glasnost/resolvers/utils.ex
@@ -1,5 +1,12 @@
 defmodule Glasnost.ResolverUtils do
-  def select_schema(blockchain) when is_binary(blockchain) do
+  def select_schema(blockchain, :comment) when is_binary(blockchain) do
+    case blockchain do
+      "steem" -> Glasnost.Steem.Comment
+      "golos" -> Glasnost.Golos.Comment
+    end
+  end
+
+  def select_schema(blockchain, :block) when is_binary(blockchain) do
     case blockchain do
       "steem" -> Glasnost.Steem.Comment
       "golos" -> Glasnost.Golos.Comment
